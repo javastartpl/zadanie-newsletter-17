cfba87f9e121635f13237e625d973113d737f0fa
slawekludwiczak <slawek@javastart.pl>
2019-07-03 14:30:34 +0200

    Init zadania

---
+++ src/main/java/pl/javastart/newsletter/Newsletter18.java
@@ -0,0 +1,27 @@
+package pl.javastart.newsletter;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+class Newsletter18 {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new TreeSet<>();
+        set1.add(10);
+        set1.add(15);
+        set1.add(5);
+        set1.add(20);
+        set1.add(5);
+        Set<Integer> set2 = new TreeSet<>();
+        set2.add(6);
+        set2.add(9);
+        set2.add(3);
+        set2.add(9);
+        set2.add(12);
+
+        System.out.println(set1); //[5, 10, 15, 20]
+        System.out.println(set2); //[3, 6, 9, 12]
+        //tutaj dodatkowo powinna wyświetlić się liczba wywołań metody add
+        //w przypadku dodania dodatkowych wywołań metody add na zbiorach set1 lub set2
+        //wartość ta powinna się automatycznie zaktualizować
+    }
+}
